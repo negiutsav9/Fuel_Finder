2b4dccf23f02ed4f1e2986825ef48c03cb09cc74
Utsav Negi <55469943+negiutsav9@users.noreply.github.com>
2023-03-13 21:47:56 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/fuelfinder/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.fuelfinder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Removing Action Bar
+        getSupportActionBar().hide();
+
+        // Initialize and assign variable
+        NavigationBarView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.map);
+
+        bottomNavigationView.setOnItemSelectedListener( new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.dashboard:
+                        startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.map:
+                        return true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(),User.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/fuelfinder/User.java
@@ -0,0 +1,49 @@
+package com.example.fuelfinder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        //Removing Action Bar
+        getSupportActionBar().hide();
+
+        // Initialize and assign variable
+        NavigationBarView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottomNavigationView.setSelectedItemId(R.id.user);
+
+        bottomNavigationView.setOnItemSelectedListener( new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.dashboard:
+                        startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.user:
+                        return true;
+                    case R.id.map:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
