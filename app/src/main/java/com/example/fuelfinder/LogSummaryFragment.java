26e9e74ed97d5798a3cbd7dec72cf4454ed8e03b
KristopherNavar <79285801+KristopherNavar@users.noreply.github.com>
2023-04-17 18:04:50 -0500

    Added graph and spinner to log summary view

--- app/src/main/java/com/example/fuelfinder/LogSummaryFragment.java
+++ app/src/main/java/com/example/fuelfinder/LogSummaryFragment.java
@@ -1,16 +1,45 @@
 package com.example.fuelfinder;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fuelfinder.databinding.FragmentLogSummaryBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Log;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 
 public class LogSummaryFragment extends Fragment {
+    GraphView graphView;
 
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    TextView test;
+    String test2 = "Foo bar";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +49,67 @@ public class LogSummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_summary, container, false);
+        View returnValue = inflater.inflate(R.layout.fragment_log_summary, container, false);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        Spinner spinner = returnValue.findViewById(R.id.logDropdown);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.logDropdownOptions, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        graphView = returnValue.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0,1),
+                new DataPoint(1,3),
+                new DataPoint(2,4),
+                new DataPoint(3,9),
+                new DataPoint(4,6),
+                new DataPoint(5,3),
+                new DataPoint(6,6),
+                new DataPoint(7,1),
+                new DataPoint(8,2)
+        });
+        graphView.addSeries(series);
+        series.setColor(Color.parseColor("#F44336"));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                graphView.setTitle(selectedItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        firebaseFirestore.collection("User").document(firebaseAuth.getUid()).collection("Logs").orderBy("timestamp", Query.Direction.DESCENDING)
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> logData = queryDocumentSnapshots.getDocuments();
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            for (DocumentSnapshot document : logData) {
+                                // after getting this list we are passing
+                                // that list to our object class.
+                                LogModel log = document.toObject(LogModel.class);
+
+                            }
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        return returnValue;
     }
 }
